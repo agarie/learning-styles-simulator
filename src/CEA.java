ff954d0ae2ae7db7dee13f2001ccee64f9106999
Carlos Agarie <carlos.agarie@gmail.com>
2014-04-24 16:57:48 -0300

    Reorganized class CEA and fixed encoding

--- src/CEA.java
+++ src/CEA.java
@@ -1,11 +1,45 @@
-
-public class CEA
-{
+public class CEA {
     private int[] cea = new int[4]; //0 = ativo, 1 = reflexivo.
     private char[] ceaSt = new char[4]; //strength - F = forte / M = moderada / L = leve
 
-    //Strong Preference Not Satisfied
-    //ceaSt -> F = forte / M = moderada / L = leve
+    public CEA(){
+        super();
+    }
+
+    public CEA(String lsc)
+    {
+        for (int i = 0; i < 4; i++){
+            this.cea[i] = Integer.parseInt(lsc.substring(i,i+1));
+        }
+    }
+
+    public void set(int d1,int d2, int d3, int d4){
+        this.cea[0] = d1;
+        this.cea[1] = d2;
+        this.cea[2] = d3;
+        this.cea[3] = d4;
+    }
+
+    public void set(int pos, int val){
+        this.cea[pos] = val;
+    }
+
+    public void setStrength(char s1, char s2, char s3, char s4){
+        this.ceaSt[0] = s1;
+        this.ceaSt[1] = s2;
+        this.ceaSt[2] = s3;
+        this.ceaSt[3] = s4;
+    }
+
+   public char[] getSt(){
+        return this.ceaSt;
+    }
+
+    public int[] get(){
+        return this.cea;
+    }
+
+    // Strong Preference Not Satisfied ceaSt: F = forte, M = moderada, L = leve
     public boolean spNotSatisfied(CEA cea) {
         boolean result = false;
         for (int i = 0; i < 4; i++){
@@ -17,8 +51,7 @@ public class CEA
         return result;
     }
 
-    //Moderate Preference Not Satisfied
-    //ceaSt -> F = forte / M = moderada / L = leve
+    // Moderate Preference Not Satisfied ceaSt: F = forte, M = moderada, L = leve
     public boolean mpNotSatisfied(CEA cea){
         boolean result = false;
         for (int i = 0; i < 4; i++){
@@ -61,45 +94,4 @@ public class CEA
         else
             System.out.println("Global");
     }
-
-    public void setStrength(char s1, char s2, char s3, char s4){
-        this.ceaSt[0] = s1;
-        this.ceaSt[1] = s2;
-        this.ceaSt[2] = s3;
-        this.ceaSt[3] = s4;
-    }
-
-   public char[] getSt(){
-        return this.ceaSt;
-    }
-
-    public int[] get(){
-        return this.cea;
-    }
-
-    public CEA(){
-        super();
-    }
-
-
-    public CEA(String lsc)
-    {
-        for (int i = 0; i < 4; i++){
-            this.cea[i] = Integer.parseInt(lsc.substring(i,i+1));
-        }
-    }
-
-
-    public void set(int d1,int d2, int d3, int d4){
-        this.cea[0] = d1;
-        this.cea[1] = d2;
-        this.cea[2] = d3;
-        this.cea[3] = d4;
-    }
-
-    public void set(int pos, int val){
-        this.cea[pos] = val;
-    }
-
-
 }
