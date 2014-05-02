59e7140f4b268229ec025bc12eec5be145afe6d5
Carlos Agarie <carlos.agarie@gmail.com>
2014-05-02 02:02:28 -0300

    Refactored StudentModel
    (well, actually ModeloEstudante)

--- src/ModeloEstudante.java
+++ src/ModeloEstudante.java
@@ -1,26 +1,28 @@
 import java.util.ArrayList;
 
+/**
+ * Represents the knowledge about a given student.
+ * <p>
+ * This model uses Felder-Silverman's learning styles to represent the preferences
+ * of a given student.
+ */
 public class ModeloEstudante {
     // Alterado durante o processo de aprendizagem.
     private EstiloAprendizagem EA;
     final private ArrayList<ObjetivoAprendizagem> OA = new ArrayList<ObjetivoAprendizagem>();
     final private ArrayList<EstadoCognitivo> EC = new ArrayList<EstadoCognitivo>();
 
-    final private double MAXDIFF = 0.9;
-    private int TAM = 60; // Quantidade de conceitos a serem aprendidos.
-    private double K = 0.5; // Taxa de aprendizagem
-    private double LIM = 0.05;
+    final private double MAX_DIFFERENCE = 0.9;
+    final private int TAM; // Quantidade de conceitos a serem aprendidos.
+    final private double K; // Taxa de aprendizagem
+    final private double limit;
 
     public ModeloEstudante(EstiloAprendizagem ea, int t, double k, double lim) {
+        // Usual values for (t, k, lim) = (60, 0.5, 0.05).
         this.EA = ea;
         this.TAM = t;
         this.K = k;
-
-        //d1; //0 = ativo, 1 = reflexivo.
-        //d2; //0 = sensitivo, 1 = intuitivo.
-        //d3; //0 = visual, 1 = verbal.
-        //d4; //0 = sequencial, 1 = global.
-        this.LIM = lim;
+        this.limit = lim;
     }
 
     public ObjetivoAprendizagem getOA(int i) {
@@ -66,9 +68,8 @@ public class ModeloEstudante {
         //Synthesis: Builds a structure or pattern from diverse elements.
         //Put parts together to form a whole, with emphasis on creating a new meaning
         //or structure.
-        for (i=0; i < TAM; i++){
+        for (i=0; i < TAM; i++) {
             c = new Conceito(i, "Conceito"+i);
-            //nc = Utilitario.RandomInt(3)+2;
             OA.add(new ObjetivoAprendizagem(c, nc));
         }
     }
@@ -77,7 +78,7 @@ public class ModeloEstudante {
         int i;
         Conceito c;
         int nc;
-        for (i=0; i < TAM; i++){
+        for (i=0; i < TAM; i++) {
             c = new Conceito(i, "Conceito"+i);
             nc = -1; //"SEM CONHECIMENTO"
             EC.add(new EstadoCognitivo(c,nc));
@@ -87,8 +88,8 @@ public class ModeloEstudante {
     public boolean OAsatisfeito() {
         int i;
         boolean resultado = true;
-        for (i=0; i < TAM; i++){
-            if (OA.get(i).getNC() > EC.get(i).getNC()){
+        for (i=0; i < TAM; i++) {
+            if (OA.get(i).getNC() > EC.get(i).getNC()) {
                 resultado = false;
                 break;
             }
@@ -96,111 +97,79 @@ public class ModeloEstudante {
         return resultado;
     }
 
+    /**
+     * Test if the cognitive level of concept i achieved the objective.
+     *
+     * @param i Index of the concept.
+     */
     public boolean conceitoSatisfeito(int i) {
-        boolean resultado = true;
-        if (OA.get(i).getNC() > EC.get(i).getNC()){
-            resultado = false;
+        if (OA.get(i).getNC() > EC.get(i).getNC()) {
+            return false
         }
-        return resultado;
+        return true;
     }
 
-    public double calcF(double DLS, double PFM) {
-        if (DLS < 0) DLS = DLS*(-1); //modudo de DLS
-        double f = (1/(DLS*PFM));
-        if (f > LIM) f = LIM;
-        return f;
+    public double calcF(double learningStylesDistance, double performance) {
+        double f = 1.0 / (Math.abs(learningStylesDistance) * performance);
+
+        // The value of `f` can't be larger than the `limit`.
+        return Math.min(f, this.limit);
     }
 
-    //PFM = performance
-    public void updateEA_badEval(CEA cea, double PFM) {
-        // CALCULO DO REFORCO.
-        double R;   //reforco
-        double DLS; //amortizador. --distancia learning styles. quanto maior a distacia entre os estilos, menor o ajuste
+    public void updateEA_badEval(CEA cea, double performance) {
+        double reinforcement;
+        //--distancia learning styles. quanto maior a distacia entre os estilos, menor e' o ajuste
+        double DLS;
 
         for (int i = 0; i < 4 ; i++) {
             DLS = this.EA.get()[i][0] - this.EA.get()[i][1];
-            R = K * calcF(DLS,PFM);
+            reinforcement = K * calcF(DLS,performance);
 
-            // APLICANDO REFORCO.
-            //regras para atualizacao do EA
-            //se a CEA esta ativo e o desempenho foi ruim, entao ele nao e' tao ativo
-            if  (cea.get()[i] == 0)
-                if ( (R + this.EA.get()[i][1]) < MAXDIFF )
-                    this.EA.set(i,this.EA.get()[i][0]-R, this.EA.get()[i][1]+R);
+            // Aplicando reforço -- regras para atualizacao do EA.
+            // Se a CEA esta ativo e o desempenho foi ruim, entao ele nao e'
+            // tao ativo.
+            if  (cea.get()[i] == 0) {
+                if ((reinforcement + this.EA.get()[i][1]) < MAX_DIFFERENCE) {
+                    this.EA.set(i,this.EA.get()[i][0]-reinforcement, this.EA.get()[i][1]+reinforcement);
+                }
+            }
 
             //se a CEA esta' reflexivo e o desempenho foi ruim, entao ele nao e' tao reflexivo
-            if  (cea.get()[i] == 1)
-                if ( (R + this.EA.get()[i][0]) < MAXDIFF )
-                    this.EA.set(i,this.EA.get()[i][0]+R, this.EA.get()[i][1]-R);
-            /*
-            //se o ME e' ativo e o CEA esta' ativo e mesmo assim o desempenho foi ruim, entao ele nao e' tao ativo
-            if ( (cea.get()[i] == 0) && (this.EA.get()[i][0] >= this.EA.get()[i][1])) {
-            this.EA.set(i,this.EA.get()[i][0]-f, this.EA.get()[i][1]+f);
+            if  (cea.get()[i] == 1) {
+                if ((reinforcement + this.EA.get()[i][0]) < MAX_DIFFERENCE) {
+                    this.EA.set(i,this.EA.get()[i][0]+reinforcement, this.EA.get()[i][1]-reinforcement);
+                }
             }
-
-            //se o ME e' reflexivo e o CEA esta' reflexivo e mesmo assim o desempenho foi ruim, entao ele nao e' tao reflexivo
-            if ( (cea.get()[i] == 1) && (this.EA.get()[i][0] <= this.EA.get()[i][1])) {
-            this.EA.set(i,this.EA.get()[i][0]+f, this.EA.get()[i][1]-f);
-            }
-
-            //se o ME e' reflexivo e o CEA esta' ativo, e seu desempenho foi ruim, entao realmente ele nao e' ativo
-            //reforca o reflexivo
-            if ( (cea.get()[i] == 0) && (this.EA.get()[i][0] <= this.EA.get()[i][1])) {
-            this.EA.set(i,this.EA.get()[i][0]-f, this.EA.get()[i][1]+f);
-            }
-
-            //se o ME e' ativo e o CEA esta' reflexivo e o desempenho foi ruim, entao realmente ele nao e' reflexivo
-            //reforca o ativo
-            if ( (cea.get()[i] == 1) && (this.EA.get()[i][0] >= this.EA.get()[i][1])) {
-            this.EA.set(i,this.EA.get()[i][0]+f, this.EA.get()[i][1]-f);
-            }
-            */
         }
     }
 
-    public void updateEA_goodEval(CEA cea, double PFM){
-        //---------CALCULO DO REFORCO-----------
-        double R;   //Reforco
-        double DLS; //--distancia learning styles. quanto maior a distacia entre os estilos, menor e' o ajuste
+    public void updateEA_goodEval(CEA cea, double performance) {
+        double reinforcement;
+        //--distancia learning styles. quanto maior a distacia entre os estilos, menor e' o ajuste
+        double DLS;
 
-        for (int i = 0; i < 4 ; i++){
+        for (int i = 0; i < 4 ; i++) {
             DLS = this.EA.get()[i][0] - this.EA.get()[i][1];
-            R = K*calcF(DLS,PFM);
+            reinforcement = K*calcF(DLS,performance);
 
             //---------APLICANDO REFORCO-----------
             //se CEA esta' ativo e ele teve um bom desempenho, entao realmente ele e' ativo
-            if (cea.get()[i] == 0)
-                if ( (R + this.EA.get()[i][0] ) < MAXDIFF )
-                    this.EA.set(i,this.EA.get()[i][0]+R, this.EA.get()[i][1]-R);
+            if (cea.get()[i] == 0) {
+                if ( (reinforcement + this.EA.get()[i][0] ) < MAX_DIFFERENCE ) {
+                     this.EA.set(i,this.EA.get()[i][0]+reinforcement, this.EA.get()[i][1]-reinforcement);
+               }
+            }
 
             //se o CEA esta' reflexivo e ele teve um bom desempenho, entao realmente ele e' reflexivo
-            if (cea.get()[i] == 1)
-                if ( (R + this.EA.get()[i][1] ) < MAXDIFF )
-                    this.EA.set(i,this.EA.get()[i][0]-R, this.EA.get()[i][1]+R);
-            /*
-            //se o ME e' ativo e o CEA esta' ativo e ele teve um bom desempenho, entao realmente ele e' ativo
-            if ( (cea.get()[i] == 0) && (this.EA.get()[i][0] >= this.EA.get()[i][1]))
-            if ( (this.EA.get()[i][0] - this.EA.get()[i][1]) < MAXDIFF )
-            this.EA.set(i,this.EA.get()[i][0]+f, this.EA.get()[i][1]-f);
-
-            //se o ME e' reflexivo e o CEA esta' reflexivo e ele teve um bom desempenho, entao realmente ele e' reflexivo
-            if ( (cea.get()[i] == 1) && (this.EA.get()[i][0] <= this.EA.get()[i][1]))
-            if ( (this.EA.get()[i][1] - this.EA.get()[i][0]) < MAXDIFF )
-            this.EA.set(i,this.EA.get()[i][0]-f, this.EA.get()[i][1]+f);
-
-            //se o ME e' reflexivo e o CEA esta' ativo, e ele teve um bom desempenho, entao  ele e' mais ativo do que parece
-            if ( (cea.get()[i] == 0) && (this.EA.get()[i][0] <= this.EA.get()[i][1]))
-            this.EA.set(i, this.EA.get()[i][0]+f, this.EA.get()[i][1]-f);
-
-            //se o ME e' ativo e o CEA esta' reflexivo e ele teve um bom desempenho, entao ele e' mais reflexivo do que aparenta
-            if ( (cea.get()[i] == 1) && (this.EA.get()[i][0] >= this.EA.get()[i][1]))
-            this.EA.set(i, this.EA.get()[i][0]-f, this.EA.get()[i][1]+f);
-            */
-
+            if (cea.get()[i] == 1) {
+                if ( (reinforcement + this.EA.get()[i][1] ) < MAX_DIFFERENCE ) {
+                    this.EA.set(i,this.EA.get()[i][0]-reinforcement, this.EA.get()[i][1]+reinforcement);
+                }
+            }
         }
     }
 
-    public void showEC(){
+    public void showEC() {
         for (EstadoCognitivo ec : EC) {
             System.out.println(ec.toString());
         }
@@ -209,7 +178,7 @@ public class ModeloEstudante {
     //compara e calcula as diferencas entre o ME e a CEA passada como parametro
     public int compare(CEA cea) {
         int diff = 0;
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++) {
             if ( (this.EA.get()[i][0] > this.EA.get()[i][1]) && (cea.get()[i] == 1) ) diff++;
             if ( (this.EA.get()[i][0] < this.EA.get()[i][1]) && (cea.get()[i] == 0) ) diff++;
         }
@@ -217,20 +186,26 @@ public class ModeloEstudante {
     }
 
     public boolean spNotSatisfied(CEA cea) {
-        boolean result = false;
-        for (int i = 0; i < 4; i++){
-            if ( (this.EA.get()[i][0] > this.EA.get()[i][1]) && (cea.get()[i] == 1) && (cea.getSt()[i] == 'F') ) {result = true; break;};
-            if ( (this.EA.get()[i][0] < this.EA.get()[i][1]) && (cea.get()[i] == 0) && (cea.getSt()[i] == 'F') ) {result = true; break;};
+        for (int i = 0; i < 4; i++) {
+            if ( (this.EA.get()[i][0] > this.EA.get()[i][1]) && (cea.get()[i] == 1) && (cea.getSt()[i] == 'F') ) {
+                return true;
+            }
+            if ( (this.EA.get()[i][0] < this.EA.get()[i][1]) && (cea.get()[i] == 0) && (cea.getSt()[i] == 'F') ) {
+                return true;
+            }
         }
-        return result;
+        return false;
     }
 
     public boolean mpNotSatisfied(CEA cea) {
-        boolean result = false;
-        for (int i = 0; i < 4; i++){
-            if ( (this.EA.get()[i][0] > this.EA.get()[i][1]) && (cea.get()[i] == 1) && (cea.getSt()[i] == 'M') ) {result = true; break;};
-            if ( (this.EA.get()[i][0] < this.EA.get()[i][1]) && (cea.get()[i] == 0) && (cea.getSt()[i] == 'M') ) {result = true; break;};
+        for (int i = 0; i < 4; i++) {
+            if ( (this.EA.get()[i][0] > this.EA.get()[i][1]) && (cea.get()[i] == 1) && (cea.getSt()[i] == 'M') ) {
+                return true;
+            }
+            if ( (this.EA.get()[i][0] < this.EA.get()[i][1]) && (cea.get()[i] == 0) && (cea.getSt()[i] == 'M') ) {
+                return true;
+            }
         }
-        return result;
+        return false;
     }
 }
